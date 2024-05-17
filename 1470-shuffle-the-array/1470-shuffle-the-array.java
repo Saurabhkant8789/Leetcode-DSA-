50bc3a8bd1c934449a5c1d174737868db4afb722
SAURABH KANT <118042533+Saurabhkant8789@users.noreply.github.com>
2024-05-17 17:47:55 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (33.48%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int j = nums.length -n;
+        int j = n;
         int k = 0;
         int [] ans = new int [nums.length];
         for(int i=0; i<nums.length; i++){
