1c6cffaf3c432be418edac95e651dc31e8155a68
SAURABH KANT <118042533+Saurabhkant8789@users.noreply.github.com>
2024-06-05 15:28:56 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (62.30%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,15 +3,15 @@ class Solution {
         
         long ans =0;
         while(x!=0){
-        long temp =0;
-            temp = (long)x % 10;
-             ans = ans*10 + temp;
+            long temp = x%10;
+            ans = ans*10 + temp;
             x = x/10;
-            
         }
-        if(ans> 2147483647 || ans< -2147483648)
+        if(ans > 2147483647 || ans < -2147483648){
             return 0;
+        }
+         return (int) ans;
         
-        return (int)ans;
     }
+   
 }
