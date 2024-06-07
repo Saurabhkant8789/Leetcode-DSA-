d2f06f829a5b19a77ed68250dc198e65435416c8
SAURABH KANT <118042533+Saurabhkant8789@users.noreply.github.com>
2024-06-07 13:08:29 +0530

    Time: 392 ms (5.71%), Space: 45.7 MB (25.98%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String frequencySort(String s) {
+        String ans = "";
+        int[] count = new int[128]; // Use 128 as the array length to cover all ASCII characters
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++; // Increment count based on the ASCII value of the character
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = frequency(count);
+            for (int j = 0; j < count[c]; j++) {
+                ans = ans + c;
+            }
+            count[c] = 0;
+        }
+        return ans;
+    }
+
+    public static char frequency(int[] sau) {
+        int index = 0;
+        int max = sau[0];
+        for (int i = 1; i < sau.length; i++) {
+            if (max < sau[i]) {
+                max = sau[i];
+                index = i;
+            }
+        }
+        return (char) index;
+    }
+}
