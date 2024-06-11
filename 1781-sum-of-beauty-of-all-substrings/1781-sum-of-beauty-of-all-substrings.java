cd197d44b713bf90254ba4c739a8683b10a72b1c
SAURABH KANT <118042533+Saurabhkant8789@users.noreply.github.com>
2024-06-11 17:31:18 +0530

    Time: 235 ms (28.04%), Space: 44.8 MB (19.96%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int beautySum(String s) {
+        
+        int sum = 0;
+        for(int i=0; i<s.length(); i++){
+            int [] newarr = new int[128];
+            
+            for( int j=i; j<s.length(); j++){
+                newarr [(int)s.charAt(j)]++; 
+                sum = sum + beauty(newarr);
+                
+            }
+        }
+        return sum;
+    }
+    
+    
+    public static int beauty (int[] arr){
+        int max = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i =0; i<arr.length; i++){
+            max = Math.max(arr[i],max);
+            if(arr[i]>0){     
+            min = Math.min(arr[i],min);}
+        }
+        return max-min;
+    }
+}
