0a9a2747f76c17f857a69153a3b87c452d95fd1c
SAURABH KANT <118042533+Saurabhkant8789@users.noreply.github.com>
2024-07-05 16:03:14 +0530

    Time: 5 ms (29.60%), Space: 46.6 MB (6.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,34 +1,9 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i =0;
-        int j=0;
-        int[] store = new int[nums1.length+nums2.length];
-        int index =0;
-        while (i<nums1.length && j<nums2.length){
-            if(nums1[i] <= nums2[j]){
-                store[index] = nums1[i];
-                i++;
-                index++;
-            }
-            else { store[index] = nums2[j];
-            j++;
-            index++;
-        }
-        }
-        if(i<nums1.length){
-            for(int s =i; s<nums1.length;s++)
-            {
-                store[index] = nums1[s];
-                index++;
-            }
-        }
-        if(j<nums2.length){
-            for(int s =j; s<nums2.length;s++)
-            {
-                store[index] = nums2[s];
-                index++;
-            }
-        }
+       int [] store = new int [nums1.length+nums2.length];
+        System.arraycopy(nums1,0,store,0,nums1.length);
+        System.arraycopy(nums2,0,store,nums1.length,nums2.length);
+        Arrays.sort(store);
         
         if(store.length%2==0){
             return (double) ((double)store[store.length/2]+(double)store[store.length/2 -1])/2;
