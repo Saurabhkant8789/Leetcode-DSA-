021a488c8ad8c9c5dc4daab2a176144f2269e33b
SAURABH KANT <118042533+Saurabhkant8789@users.noreply.github.com>
2024-07-05 15:54:31 +0530

    Time: 2 ms (62.61%), Space: 45.7 MB (67.82%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] new_arr = new int[n];
-
-        int i=0, j=0, k=0;
-
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) new_arr[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) new_arr[k++] = nums1[i++];
-                break;
+        int i =0;
+        int j=0;
+        int[] store = new int[nums1.length+nums2.length];
+        int index =0;
+        while (i<nums1.length && j<nums2.length){
+            if(nums1[i] <= nums2[j]){
+                store[index] = nums1[i];
+                i++;
+                index++;
             }
-
-            if (nums1[i] < nums2[j]) {
-                new_arr[k++] = nums1[i++];
-            } else {
-                new_arr[k++] = nums2[j++];
+            else { store[index] = nums2[j];
+            j++;
+            index++;
+        }
+        }
+        if(i<nums1.length){
+            for(int s =i; s<nums1.length;s++)
+            {
+                store[index] = nums1[s];
+                index++;
             }
         }
-
-        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-        else return new_arr[n/2];
+        if(j<nums2.length){
+            for(int s =j; s<nums2.length;s++)
+            {
+                store[index] = nums2[s];
+                index++;
+            }
+        }
+        
+        if(store.length%2==0){
+            return (double) ((double)store[store.length/2]+(double)store[store.length/2 -1])/2;
+        }
+        return (double) store[store.length/2];
     }
 }
